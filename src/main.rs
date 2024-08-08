ac9845fe65f8c465b83fe3f2c6b264901b9746f4
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2024-08-08 22:49:26 +0300

    dependencies updated

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ mod models;
 mod router;
 mod system_models;
 
-use axum::Server;
-
 #[tokio::main]
 async fn main() {
 	let app_state = db::create_db_connection().await;
 	let app = router::create_router(app_state);
 
-	let binded = Server::bind(&config::get_http_host_to_serve());
+	let binded = tokio::net::TcpListener::bind(&config::get_http_host_to_serve())
+		.await
+		.unwrap();
 	println!(":) Server started successfully");
-	binded.serve(app.into_make_service()).await.unwrap();
+	axum::serve(binded, app).await.unwrap();
 }
