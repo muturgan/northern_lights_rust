76bf562edbfc093ce77ade573f0552e247ed397c
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2024-08-15 03:14:31 +0300

    graceful_shutdown

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,36 @@ async fn main() {
 	let app_state = db::create_db_connection().await;
 	let app = router::create_router(app_state);
 
-	let binded = tokio::net::TcpListener::bind(&config::get_http_host_to_serve())
+	async fn shutdown_signal() {
+		let ctrl_c = async {
+			tokio::signal::ctrl_c()
+				.await
+				.expect("failed to install Ctrl+C handler");
+		};
+
+		#[cfg(unix)]
+		let terminate = async {
+			tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+				.expect("failed to install signal handler")
+				.recv()
+				.await;
+		};
+
+		#[cfg(not(unix))]
+		let terminate = std::future::pending::<()>();
+
+		tokio::select! {
+			_ = ctrl_c => {},
+			_ = terminate => {},
+		}
+	}
+
+	let listener = tokio::net::TcpListener::bind(&config::get_http_host_to_serve())
 		.await
 		.unwrap();
 	println!(":) Server started successfully");
-	axum::serve(binded, app).await.unwrap();
+	axum::serve(listener, app)
+		.with_graceful_shutdown(shutdown_signal())
+		.await
+		.unwrap();
 }
