ad03c7fbd3ba4a0e63ad4e0f5b807b84c9ea2a54
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2024-08-17 22:22:27 +0300

    a "start" script was added

--- scripts.sh
+++ scripts.sh
@@ -8,6 +8,15 @@ dev() {
 	export $(cat .env | xargs) && cargo run
 }
 
+start_release() {
+	export $(cat .env | xargs) && ./target/release/promo_codes
+}
+
+start() {
+	build;
+	start_release;
+}
+
 clippy() {
 	cargo clippy --all --all-features --tests -- -D warnings;
 	echo "clippy is ok";
