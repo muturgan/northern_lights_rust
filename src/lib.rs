a6e61c30cc0391289c949bb7fb5b4d716c0993a9
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2024-08-18 13:00:47 +0300

    lib just exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,10 @@
 #![allow(clippy::needless_return)]
 
 pub mod config;
-mod db;
-mod dto;
-mod handler;
-mod models;
-mod repo;
-mod router;
-mod system_models;
-
-pub async fn start() {
-	let app_state = db::create_db_connection().await;
-	let app = router::create_router(app_state);
-
-	async fn shutdown_signal() {
-		let ctrl_c = async {
-			tokio::signal::ctrl_c()
-				.await
-				.expect("failed to install Ctrl+C handler");
-		};
-
-		#[cfg(unix)]
-		let terminate = async {
-			tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
-				.expect("failed to install signal handler")
-				.recv()
-				.await;
-		};
-
-		#[cfg(not(unix))]
-		let terminate = std::future::pending::<()>();
-
-		tokio::select! {
-			_ = ctrl_c => {},
-			_ = terminate => {},
-		}
-	}
-
-	let listener = tokio::net::TcpListener::bind(&config::get_http_host_to_serve())
-		.await
-		.unwrap();
-	println!(":) Server started successfully");
-	axum::serve(listener, app)
-		.with_graceful_shutdown(shutdown_signal())
-		.await
-		.unwrap();
-}
+pub mod db;
+pub mod dto;
+pub mod handler;
+pub mod models;
+pub mod repo;
+pub mod router;
+pub mod system_models;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,44 @@
 #![allow(clippy::needless_return)]
 
-use promo_codes;
+use promo_codes::config;
+use promo_codes::db;
+use promo_codes::router;
 
 #[tokio::main]
 async fn main() {
-	promo_codes::start().await;
+	let app_state = db::create_db_connection().await;
+	let app = router::create_router(app_state);
+
+	async fn shutdown_signal() {
+		let ctrl_c = async {
+			tokio::signal::ctrl_c()
+				.await
+				.expect("failed to install Ctrl+C handler");
+		};
+
+		#[cfg(unix)]
+		let terminate = async {
+			tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+				.expect("failed to install signal handler")
+				.recv()
+				.await;
+		};
+
+		#[cfg(not(unix))]
+		let terminate = std::future::pending::<()>();
+
+		tokio::select! {
+			_ = ctrl_c => {},
+			_ = terminate => {},
+		}
+	}
+
+	let listener = tokio::net::TcpListener::bind(&config::get_http_host_to_serve())
+		.await
+		.unwrap();
+	println!(":) Server started successfully");
+	axum::serve(listener, app)
+		.with_graceful_shutdown(shutdown_signal())
+		.await
+		.unwrap();
 }
