934a37e1596b8a90061d5793b90b5786de22fe49
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2024-08-19 19:19:27 +0300

    formatting

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,11 @@ async fn main() {
 	let repo = Arc::new(repo);
 	let app = router::create_router(repo.clone());
 
-	let listener = tokio::net::TcpListener::bind(&config::get_http_host_to_serve())
-		.await
-		.unwrap();
+	let addr = config::get_http_host_to_serve();
+	let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
 	println!(":) Server started successfully");
+
 	axum::serve(listener, app)
 		.with_graceful_shutdown(shutdown_signal(repo))
 		.await
