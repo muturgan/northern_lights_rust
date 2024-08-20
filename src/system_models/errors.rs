71b9504c07451b8d9602b35740184149e19c257a
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2024-08-20 23:34:34 +0300

    impl Error for AppError

--- src/system_models/errors.rs
+++ src/system_models/errors.rs
@@ -1,4 +1,16 @@
+use ::std::error::Error;
+use ::std::fmt::{Display, Formatter, Result as FmtResult};
+
+#[derive(Debug)]
 pub enum AppError {
 	ScenarioError(String, Option<String>),
 	SystemError(String),
 }
+
+impl Display for AppError {
+	fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+		write!(f, "{}", self)
+	}
+}
+
+impl Error for AppError {}
