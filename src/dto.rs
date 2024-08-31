1720c345fba3b6d0a23474489b8a66b20338d234
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2024-09-01 00:17:15 +0300

    RE_PHONE updated

--- src/dto.rs
+++ src/dto.rs
@@ -13,7 +13,7 @@ use validator::Validate;
 use crate::system_models::AppError;
 
 lazy_static! {
-	static ref RE_PHONE: Regex = Regex::new(r"^(\+7)\d{10}$").unwrap();
+	static ref RE_PHONE: Regex = Regex::new(r"^(\+79)\d{9}$").unwrap();
 	static ref RE_PROMO: Regex = Regex::new(r"^[а-я]{4,8}-\d{3}$").unwrap();
 	static ref RE_DATE: Regex = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
 }
