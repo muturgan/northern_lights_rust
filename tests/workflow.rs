83925a7063270d690c40f1491e18bf15a1e5145d
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2024-12-17 22:04:20 +0300

    an amazing integration test was added

--- tests/workflow.rs
+++ tests/workflow.rs
@@ -1,18 +1,95 @@
 use ::std::sync::Arc;
-use axum::extract::State;
-use promo_codes::*;
-use system_models::EScenarioStatus;
+use axum::http::{StatusCode, header};
+use axum_test::TestServer;
+use promo_codes::{
+	repository::{Repository, models::RegisteredUser},
+	router,
+	system_models::{AppResponse, EScenarioStatus},
+};
+use serde_json::json;
 
 #[tokio::test]
 async fn test_workflow() {
-	let repo = repository::Repository::new().await;
-	let repo = Arc::new(repo);
+	let repo = Repository::new().await;
+	let app = router::create_router(Arc::new(repo));
+	let server = TestServer::new(app).unwrap();
 
-	let state_repo = State(repo.clone());
+	// Пустой репозиторий
+	let res = server.get("/api/users").await;
+	assert_eq!(res.status_code(), StatusCode::OK);
+	let body = res.json::<AppResponse>();
+	assert_eq!(body.status, EScenarioStatus::SCENARIO_SUCCESS);
+	let users = serde_json::from_value::<Vec<RegisteredUser>>(body.payload.unwrap()).unwrap();
+	assert_eq!(users.len(), 0);
 
-	let users = handler::users(state_repo.clone()).await.unwrap();
-	assert_eq!(users.status, EScenarioStatus::SCENARIO_SUCCESS);
-	println!("{}", users.payload.unwrap());
+	// Регистрация
+	let res = server
+		.post("/api/registration")
+		.json(&json!({
+			"phone": "+79505901234",
+			"firstName": "Andrey",
+			"birthDate": "1987-01-21",
+		}))
+		.await;
+	assert_eq!(res.status_code(), StatusCode::OK);
+	let body = res.json::<AppResponse>();
+	assert_eq!(body.status, EScenarioStatus::SCENARIO_SUCCESS);
+	let mut iter = body.result.split(": ");
+	iter.next();
+	let promo = iter.next().unwrap().to_lowercase();
 
-	repo.close().await;
+	// Непустой репозиторий
+	let res = server.get("/api/users").await;
+	assert_eq!(res.status_code(), StatusCode::OK);
+	let body = res.json::<AppResponse>();
+	assert_eq!(body.status, EScenarioStatus::SCENARIO_SUCCESS);
+	let users = serde_json::from_value::<Vec<RegisteredUser>>(body.payload.unwrap()).unwrap();
+	assert_eq!(users.len(), 1);
+
+	// Ошибка авторизации
+	let res = server.post("/api/check").await;
+	assert_eq!(res.status_code(), StatusCode::OK);
+	let body = res.json::<AppResponse>();
+	assert_eq!(body.status, EScenarioStatus::UNAUTHORIZED);
+
+	// Проверка промокода
+	let pass = promo_codes::config::get_admin_pass();
+	let res = server
+		.post("/api/check")
+		.add_header(header::AUTHORIZATION, pass)
+		.json(&json!({
+			"phone": "+79505901234",
+			"promocode": &promo,
+		}))
+		.await;
+	assert_eq!(res.status_code(), StatusCode::OK);
+	let body = res.json::<AppResponse>();
+	assert_eq!(body.status, EScenarioStatus::SCENARIO_SUCCESS);
+	let auth_cookie = res.cookies().delta().next().unwrap().to_owned();
+
+	// Активация промокода
+	let res = server
+		.post("/api/activate")
+		.add_cookie(auth_cookie.clone())
+		.json(&json!({
+			"phone": "+79505901234",
+			"promocode": &promo,
+		}))
+		.await;
+	assert_eq!(res.status_code(), StatusCode::OK);
+	let body = res.json::<AppResponse>();
+	assert_eq!(body.status, EScenarioStatus::SCENARIO_SUCCESS);
+
+	// Повторная активация промокода
+	let res = server
+		.post("/api/activate")
+		.add_cookie(auth_cookie.clone())
+		.json(&json!({
+			"phone": "+79505901234",
+			"promocode": &promo,
+		}))
+		.await;
+	assert_eq!(res.status_code(), StatusCode::OK);
+	let body = res.json::<AppResponse>();
+	assert_eq!(body.status, EScenarioStatus::SCENARIO_FAIL);
 }
